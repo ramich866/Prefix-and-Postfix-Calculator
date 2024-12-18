8c2be0f666347a22d267ffbd00313889c1ef07f9
ramich866 <ramichhonor@gmail.com>
2024-12-18 20:56:50 +0200

    JS <> add change mode fuction

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
 const display = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
 
+let stack = []
+let mode = "Prefix"
+
 function addToDisplay(input) {
   display.value += input;
 }
@@ -11,4 +14,27 @@ function clearDisplay() {
 
 function deleteLast() {
   display.value = display.value.slice(0, -1) 
+}
+
+function changeMode() {
+  if (mode == "postfix") {
+    mode = "prefix";
+    document.querySelector(".display").placeholder = `MODE: ${mode}`;
+  } else {
+    mode = "postfix";
+    document.querySelector(".display").placeholder = `MODE: ${mode}`;
+  }
+  }
+
+function evaluatePrefix(expression) {
+  expression = display.value
+}
+
+function evaluatePostfix(expression) {
+
+}
+
+function evaluate() {
+  if (mode == "prefix") return evaluatePrefix
+  else return evaluatePostfix;
 }
