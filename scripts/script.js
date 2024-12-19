6a4228ea289c54c324c3c6e4c5621fa00c78058c
ramich866 <ramichhonor@gmail.com>
2024-12-19 11:22:55 +0200

    JS <> renamed function cz evaluate() is predefined + add evaluatePrefix function

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,9 @@
 const display = document.querySelector(".display");
 
 let stack = [];
-let mode = "Prefix";
+
+let mode = "prefix";
+const expression = display.value;
 
 function addToDisplay(input) {
   display.value += input;
@@ -17,25 +19,72 @@ function deleteLast() {
 
 function changeMode() {
   clearDisplay();
-  if (mode == "postfix") {
-    mode = "prefix";
-    document.querySelector(".display").placeholder = `MODE: ${mode}`;
+  if (mode == "Postfix") {
+    mode = "Prefix";
   } else {
-    mode = "postfix";
-    document.querySelector(".display").placeholder = `MODE: ${mode}`;
+    mode = "Postfix";
   }
-  }
+  document.querySelector(".display").placeholder = `MODE: ${mode}`;
+}
 
 function evaluatePrefix(expression) {
   expression = display.value;
+  
+  if (expression.length > 1 && !expression.includes(" ")) {
+    expression = "yolo"
+    console.log(expression)
+  return clearDisplay();
+}
+
+  for (let i = expression.length - 1; i >= 0; i--) {
+    if (expression[i] == ' ') continue; // skip if character is space
+    
+    if (expression[i] >= '0' && expression[i] <= '9') {
+      let number = 0;
+      let j = i;
+
+      while (i < expression.length && expression[i] >= '0' && expression[i] <= '9') {
+        i--;
+      } 
+      i++;
+
+      for (let v = i; v <= j; v++) {
+        number = number * 10 + (expression[v] - '0');
+        }
+      stack.push(number); 
+    } 
+    else { // found operator ==> pop two elements
+        let A = stack[stack.length - 1];
+        stack.pop();
+        let B = stack[stack.length - 1];
+        stack.pop();
+
+        switch (expression[i]) { // calculate operations  
+          case "+": stack.push(A + B);
+          break;
+          case "-": stack.push(A - B);
+          break;
+          case "*": stack.push(A * B);
+          break;
+          case "/": stack.push(A / B);
+          break;
+      }
+    }
+  }
+  console.log(stack);
   console.log(expression);
+  return stack[stack.length - 1];
 }
 
+//not finished
 function evaluatePostfix(expression) {
-
+  expression = display.value;
 }
 
-function evaluate() {
-  if (mode == "prefix") return evaluatePrefix(expression);
-  else return evaluatePostfix(expression);
+function calculateResult() {
+  if (mode == "prefix") evaluatePrefix(expression);
+  else evaluatePostfix(expression);
+  clearDisplay();
+  display.value = stack;
+  stack = [];
 }
