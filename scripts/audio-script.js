1cd06edc1b6978335569fa859a838810f64a3ed3
ramich866 <ramichhonor@gmail.com>
2024-12-21 05:57:13 +0200

    JS <> fix audio script

--- scripts/audio-script.js
+++ scripts/audio-script.js
@@ -1,5 +1,5 @@
 const number_buttons = document.querySelectorAll('.misc-buttons');
-const ring_sound_effect = new Audio('../Sound-effects/Sonic Ring - Sound Effect (HD).mp3');
+const ring_sound_effect = new Audio('./Sound-effects/Sonic Ring - Sound Effect (HD).mp3');
 ring_sound_effect.volume = 0.1;
 
 number_buttons.forEach(button => {
